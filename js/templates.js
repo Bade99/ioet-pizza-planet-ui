7c3ce02823b72a0779d784c8b8c36f02b2ea1187
Bade99 <31745377+Bade99@users.noreply.github.com>
2024-08-14 16:49:20 -0300

    feat/FBA - PIZ1: Add beverages and a common header ui template

---
+++ js/templates.js
@@ -0,0 +1,38 @@
+
+function createHeaderTemplate() {
+    let header = $("#header-section");
+    let template = $("#header-template")[0].innerHTML.replace("&gt;", ">");
+    let headerContent = Mustache.render(template, {}, {"header":
+    `<header class="header-section container-fluid">
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">
+            <img src="/images/image4.png" width="50" height="50" class="d-inline-block align-top" alt="" />
+            L'ultima Cena
+        </a>
+        <ul class="navbar-nav mr-auto"></ul>
+        <ul class="navbar-nav">
+            <!-- LINKS -->
+
+            <li class="nav-item active">
+            <a class="nav-link" href="/">Build Your Pizza!</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/app/order/orders.html">Orders</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/app/ingredient/ingredients.html">Ingredients</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/app/beverage/beverages.html">Beverages</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/app/size/sizes.html">Sizes</a>
+            </li>
+        </ul>
+        </nav>
+    </header>`
+    });
+    header.html(headerContent);
+}
+
+window.addEventListener("load", createHeaderTemplate, false);
